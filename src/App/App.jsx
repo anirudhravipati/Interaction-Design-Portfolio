68acd0620a60af2a8263c1b65a4ab09985c65c53
Aniruddh Ravipati <a.ravipati@edu.ciid.dk>
2023-04-02 16:06:49 +0530

    Setting Type for this project

---
+++ src/App/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello There</h1>
+      <h2>this is h2</h2>
+      <h3>this is h3</h3>
+      <h4>this is h4</h4>
+      <h5>this is h5</h5>
+      <h6>this is h6</h6>
+      <p>this is the paragraph</p>
+      <pre> this is what pre looks like</pre>
+      <footer>
+        Hello There! this is the footer
+      </footer>
+    </div>
+  )
+}
+
+export default App
