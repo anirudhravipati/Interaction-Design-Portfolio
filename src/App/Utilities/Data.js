37b4f062ca145085a41f6491aa1a9a9b6a7d0707
Aniruddh Ravipati <a.ravipati@edu.ciid.dk>
2023-06-12 23:33:26 +0530

    Added Data.js
    Data.js will function as the source of all data i.e. copy and images.
    
    Changes to be committed:
    	new file:   public/Portfolio v3/Project/dan-meyers-hA-3tgkoiIQ-unsplash.jpg
    	deleted:    public/Portfolio v3/Project/dan-meyers-tJCnVB9o9PQ-unsplash.jpg
    	deleted:    public/Portfolio v3/Project/museum-of-new-zealand-te-papa-tongarewa-7nnsjS9PnPI-unsplash.jpg
    	deleted:    public/Portfolio v3/Project/polina-kondrashova-sOuCmvooiHA-unsplash.jpg
    	new file:   public/Portfolio v3/Project/red-zeppelin-sdfiCCK99G0-unsplash.jpg
    	new file:   public/Portfolio v3/Project/zhuojun-yu-xhC-pVI_Gno-unsplash (1).jpg
    	new file:   src/App/Utilities/Data.js
    	modified:   src/App/components/Project-List/Project-list.css
    	modified:   src/App/components/Project-List/Project-list.jsx

---
+++ src/App/Utilities/Data.js
@@ -0,0 +1,27 @@
+
+export const ProjectListData = [
+  {
+    key: "title-1",
+    tooltip_key: "tooltip-title-1",
+    title: "Title 1",
+    src: "Portfolio v3/Project/dan-meyers-hA-3tgkoiIQ-unsplash.jpg",
+    content: "Nam exercitationem culpa et molestiae voluptatibus aut dolore quasi in omnis esse. Cum Quis quisquam non natus incidunt hic aliquam rerum. Et aperiam exercitationem qui itaque repellat aut fuga asperiores. Hic saepe autem nam tenetur culpa cum ipsam laborum aut corrupti ullam et quisquam labore et beatae minima quo dolores quae.",
+    tags: ["exercitationem", "culpa", "molestiae"]
+  },
+  {
+    key: "title-2",
+    tooltip_key: "tooltip-title-2",
+    title: "Title 2",
+    src: "Portfolio v3/Project/zhuojun-yu-xhC-pVI_Gno-unsplash (1).jpg",
+    content: "Ex possimus quas eum atque quia est beatae eligendi est animi amet. Ut deleniti eligendi ea nihil necessitatibus est recusandae corrupti qui earum nostrum eos autem sequi.",
+    tags: ["possimus", "atque", "eligendi"]
+  },
+  {
+    key: "title-3",
+    tooltip_key: "tooltip-title-3",
+    title: "Title 3",
+    src: "Portfolio v3/Project/red-zeppelin-sdfiCCK99G0-unsplash.jpg",
+    content: "Aut neque distinctio ut tempora earum vel corporis corporis et ipsam praesentium. Et minima adipisci ex amet reprehenderit est illum internos ut distinctio neque hic atque architecto eum officiis exercitationem.",
+    tags: ["distinctio", "tempora", "corporis"]
+  }
+]
--- src/App/components/Project-List/Project-list.jsx
+++ src/App/components/Project-List/Project-list.jsx
@@ -1,69 +1,72 @@
 import "./Project-list.css";
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { autoPlacement,useFloating,autoUpdate } from "@floating-ui/react";
+import { offset, flip, useFloating, autoUpdate } from "@floating-ui/react";
 import { useEffect, useState } from "react";
-
+import { ProjectListData } from "../../Utilities/Data";
 
 function ProjectList() {
+
   return (
     <motion.div layout transition={{ type: "spring" }} id="Projects">
       <h2> Projects</h2>
       <motion.div id="projects-list">
-        <ProjectItem title="CloudAEye"/>
-        <ProjectItem title="FAM"/>
-        <ProjectItem title="CloudAEye"/>
+        {
+          ProjectListData.map((project) => <ProjectItem item={project} />)
+        }
       </motion.div>
     </motion.div>
   );
 }
 
-function ProjectItem({title="Title"})
-{
-  const [anchor,setAnchor] = useState(null);
-  const [hoverState,setHover] = useState(false);
+function ProjectItem({ item }) {
+  const [anchor, setAnchor] = useState(null);
+  const [hoverState, setHover] = useState(false);
 
   return (
-    <motion.div ref={setAnchor} onHoverStart={()=>setHover(true)} onHoverEnd={()=>setHover(false)} className="project-item">
-      <h4>{title}</h4>
-      <Tooltip hover={hoverState} anchor={anchor}/>
+    <motion.div key={item.key} ref={setAnchor} onHoverStart={() => setHover(true)} onHoverEnd={() => setHover(false)} className="project-item">
+      <h4>{item.title}</h4>
+      <Tooltip item={item} hover={hoverState} anchor={anchor} />
     </motion.div>
   )
 }
 
-function Tooltip({hover=false,anchor})
-{
+function Tooltip({ item, hover = false, anchor }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const {refs, floatingStyles} = useFloating(
+  const { refs, floatingStyles } = useFloating(
     {
       open: isOpen,
       onOpenChange: setIsOpen,
       placement: "right",
+      whileElementsMounted: autoUpdate,
+      middleware: [offset(-4), flip()],
       elements: {
         reference: anchor,
       }
     }
   );
 
-  useEffect( ()=> setIsOpen(hover)
+  useEffect(() => setIsOpen(hover)
     , [hover]
   )
 
-  console.log(`isOpen :`+isOpen)
-  return(
+  console.log(`isOpen :` + isOpen)
+  return (
     <>
-    {isOpen && (
-      <div ref={refs.setFloating} style={floatingStyles}>
-        <div  className="project-list-tooltip">
-            ToolTip goes here!
+      {isOpen && (
+        <div key={item.tooltip_key} ref={refs.setFloating} style={floatingStyles}>
+          <div className="project-list-tooltip">
+            <h6>{item.title}</h6>
+            <img rel="preload" src={item.src} className="preview-image" />
+            <p>{item.content}</p>
+            <div className="tags">
+              {item.tags.map(tag => <div className="tag">tag</div>)}
+            </div>
+          </div>
         </div>
-      </div>
-    )}
+      )}
     </>
   )
-
-
 }
 
 export default ProjectList;
